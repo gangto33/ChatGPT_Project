d422cca80ae95b8a29ed742fa75f46a832a95d3e
gangto33 <142385671+gangto33@users.noreply.github.com>
2023-11-30 20:58:20 +0900

    Update settings.py

--- chatgpt-project/ChatProject/settings.py
+++ chatgpt-project/ChatProject/settings.py
@@ -12,16 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 from datetime import timedelta
-import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-env = environ.Env()
-env_file = str(BASE_DIR.joinpath(".env"))
-environ.Env.read_env(env_file)
-
-OPENAI_API_KEY = env("OPENAI_API_KEY")
 
 
 # Quick-start development settings - unsuitable for production
